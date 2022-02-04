49b0e4532c4bccd020847a89b3880ddd59b80736
vincent-tr <vincent.tr@gmail.com>
2022-02-04 10:04:04 +0100

    first working i2c

--- epanel-beta/com.cc
+++ epanel-beta/com.cc
@@ -1,5 +1,6 @@
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
+#include "i2c_fifo.h"
 #include "common/service.hh"
 #include "common/application.hh"
 #include "common/logger.hh"
@@ -20,8 +21,60 @@ namespace mylife {
 
   static constexpr uint16_t magic = 0x4242;
 
+  com *com::s_instance = nullptr;
+
+  // all transactions are on u16
+  class transaction {
+  public:
+    transaction(uint8_t type)
+     : m_type(type) {
+       // DEBUG << "begin transaction " << static_cast<int>(type);
+    }
+
+    ~transaction() {
+      // DEBUG << "end transaction";
+    }
+
+    void set_value(uint16_t value) {
+      m_value.value = value;
+    }
+
+    uint16_t get_value() const {
+      return m_value.value;
+    }
+
+    uint8_t get_byte() {
+      assert(m_offset < 2);
+      return m_value.parts[m_offset++];
+    }
+
+    void set_byte(uint8_t value) {
+      assert(m_offset < 2);
+      m_value.parts[m_offset++] = value;
+    }
+
+    uint8_t type() const {
+      return m_type;
+    }
+
+    bool ended() const {
+      return m_offset >= 2;
+    }
+
+  private:
+    uint8_t m_type;
+
+    union {
+      std::uint16_t value;
+      std::uint8_t  parts[2];
+    } m_value;
+
+    int m_offset = 0;
+  };
+
   com::com(uint8_t address)
    : m_address(address) {
+    s_instance = this;
   }
 
   void com::setup() {
@@ -29,60 +82,99 @@ namespace mylife {
 
     gpio_init(sda_pin);
     gpio_set_function(sda_pin, GPIO_FUNC_I2C);
-    gpio_pull_up(sda_pin);
+    // gpio_pull_up(sda_pin);
 
     gpio_init(scl_pin);
     gpio_set_function(scl_pin, GPIO_FUNC_I2C);
-    gpio_pull_up(scl_pin);
+    // gpio_pull_up(scl_pin);
 
     i2c_init(i2c0, baudrate);
-    i2c_set_slave_mode(i2c0, true, m_address);
+    i2c_slave_init(i2c0, m_address, &i2c_handler_s);
   }
 
-  static uint16_t i2c_read() {
-    uint16_t value;
-    i2c_read_raw_blocking(i2c0, reinterpret_cast<uint8_t *>(&value), sizeof(value));
-    return value;
+  void com::i2c_handler_s(i2c_inst_t *i2c, i2c_slave_event_t event) {
+    s_instance->i2c_handler(i2c, event);
   }
 
-  static void i2c_write(uint16_t value) {
-    i2c_write_raw_blocking(i2c0, reinterpret_cast<const uint8_t *>(&value), sizeof(value));
-  }
+  void com::i2c_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
+    switch (event) {
+      case I2C_SLAVE_RECEIVE: {
+        auto data = i2c_read_byte(i2c);
 
-  void com::loop() {
-    if (i2c_get_read_available(i2c0) == 0) {
-      return;
-    }
+        if (!m_transaction) {
+          init_transaction(data);
+          return;
+        }
 
-    uint8_t reg;
-    i2c_read_raw_blocking(i2c0, &reg, 1);
-    DEBUG << "got request " << static_cast<int>(reg);
+        m_transaction->set_byte(data);
 
-    switch (reg) {
-      case reg_check:
-        i2c_write(magic);
+        if (m_transaction->ended()) {
+          finish_transaction();
+        }
+
         break;
+      }
 
-      case reg_reset:
-        i2c_read();
-        m_state->reset();
+      case I2C_SLAVE_REQUEST: {
+        assert(m_transaction);
+        auto data = m_transaction->get_byte();
+        i2c_write_byte(i2c, data);
+
+        if (m_transaction->ended()) {
+          finish_transaction();
+        }
+
+        break;
+      }
+
+      case I2C_SLAVE_FINISH:
+        // esphome does not handle repeated stop, so we have to handle it though multiple queries
+        break;
+    }
+  }
+
+  void com::init_transaction(uint8_t reg) {
+    m_transaction = new transaction(reg);
+
+    switch(reg) {
+      case reg_check:
+        m_transaction->set_value(magic);
         break;
 
       case reg_inputs:
-        i2c_write(m_state->get_inputs());
+        m_transaction->set_value(m_state->get_inputs());
         break;
 
-      case reg_outputs: {
-        auto word = i2c_read();
-        auto index = (uint8_t)(word >> 8);
-        auto value = (uint8_t)(word & 0x00ff);
-        m_state->set_output(index, value);
+      case reg_reset:
+      case reg_outputs:
         break;
-      }
 
       default:
         ERROR << "got unknown request" << static_cast<int>(reg);
         break;
     }
+  }
+
+  void com::finish_transaction() {
+    switch(m_transaction->type()) {
+      case reg_check:
+      case reg_inputs:
+        break;
+
+      case reg_reset:
+        m_state->reset();
+        break;
+
+      case reg_outputs: {
+        auto word = m_transaction->get_value();
+        auto index = (uint8_t)(word >> 8);
+        auto value = (uint8_t)(word & 0x00ff);
+        m_state->set_output(index, value);
+        break;
+      }
+    }
+
+    delete m_transaction;
+    m_transaction = nullptr;
   }
 }
--- epanel-beta/com.hh
+++ epanel-beta/com.hh
@@ -1,9 +1,11 @@
 #pragma once
 
+#include "i2c_slave.h" // need this for i2c_slave_event_t (cannot forward declare it)
 #include "common/service.hh"
 
 namespace mylife {
   class state;
+  class transaction;
 
   class com : public service {
   public:
@@ -11,11 +13,17 @@ namespace mylife {
     virtual ~com() = default;
 
     virtual void setup() override;
-    virtual void loop() override;
 
   private:
+    static void i2c_handler_s(i2c_inst_t *i2c, i2c_slave_event_t event);
+    void i2c_handler(i2c_inst_t *i2c, i2c_slave_event_t event);
+    void init_transaction(uint8_t reg);
+    void finish_transaction();
+
+    static com *s_instance;
     state *m_state = nullptr;
     uint8_t m_address;
+    transaction *m_transaction = nullptr;
   };
 
 }

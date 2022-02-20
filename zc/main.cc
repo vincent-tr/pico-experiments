51986d33086f541fb6eb8e038ce2e0ae81d264f4
vincent-tr <vincent.tr@gmail.com>
2022-02-20 18:18:34 +0100

    split stuff

--- zc/main.cc
+++ zc/main.cc
@@ -1,106 +1,13 @@
 #include <iostream>
-#include <array>
-#include <functional>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
-#include "pico/time.h"
+
+#include "zc.hh"
 
 #define GPIO_ZC 13 // green
 #define GPIO_TRIAC 14 // yellow
 #define GPIO_MOSFET 15 // orange
-
-template<typename... X>
-class callback_manager;
-
-template<typename... Args>
-class callback_manager<void(Args...)> final {
-public:
-  void add(std::function<void(Args...)> &&callback) {
-    m_callbacks.emplace_back(std::move(callback));
-  }
-
-  void call(Args... args) {
-    for (const auto &callback : m_callbacks) {
-      callback(args...);
-    }
-  }
-
- private:
-  std::vector<std::function<void(Args...)>> m_callbacks;
-};
-
-class zero_crossing_detector final {
-public:
-  zero_crossing_detector(uint gpio) {
-    m_instance = this;
-
-    alarm_pool_init_default();
-
-    gpio_init(gpio);
-    gpio_set_dir(gpio, GPIO_IN);
-
-    // rising_per_sec rising per second
-    gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &s_irq_handler);
-  }
-
-  uint32_t period_us() const {
-    return m_period;
-  }
-
-  uint32_t zero_duration_us() const {
-    return m_zero_duration;
-  }
-
-  void register_callback(std::function<void()> &&callback) {
-    m_callback.add(std::move(callback));
-  }
-
-private:
-  void irq_handler(uint32_t events) {
-    auto now = get_absolute_time();
-    auto elapsed = absolute_time_diff_us(m_last_event, now);
-    m_last_event = now;
-
-    switch(events) {
-    case GPIO_IRQ_EDGE_RISE:
-      m_period = m_zero_duration + elapsed;
-
-      // middle of zero
-      add_alarm_in_us(m_zero_duration / 2, s_alarm_trigger, this, true);
-      break;
-
-    case GPIO_IRQ_EDGE_FALL:
-      m_zero_duration = elapsed;
-      break;
-    }
-  }
-
-  void alarm_trigger() {
-    m_callback.call();
-  }
-
-  static void s_irq_handler(uint gpio, uint32_t events) {
-    m_instance->irq_handler(events);
-  }
-
-  static int64_t s_alarm_trigger(alarm_id_t id, void *user_data) {
-    auto instance = reinterpret_cast<zero_crossing_detector *>(user_data);
-    instance->alarm_trigger();
-    return 0;
-  }
-
-  // this is ugly but we cannot pass args to irq handler
-  static zero_crossing_detector *m_instance;
-
-  absolute_time_t ABSOLUTE_TIME_INITIALIZED_VAR(m_last_event, 0);
-  uint32_t m_period = 0;
-  uint32_t m_zero_duration = 0;
-
-  callback_manager<void()> m_callback;
-};
-
-zero_crossing_detector *zero_crossing_detector::m_instance = nullptr;
 
 int main() {
   bi_decl(bi_program_description("Zero crossing detector tests"));

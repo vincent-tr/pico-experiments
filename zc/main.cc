cd2bd3460e5b0f41075286debba94948df055bf3
vincent-tr <vincent.tr@gmail.com>
2022-02-20 11:57:15 +0100

    zc class

--- zc/main.cc
+++ zc/main.cc
@@ -4,51 +4,88 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
+#include "pico/time.h"
 
 #define GPIO_ZC 13 // green
 #define GPIO_TRIAC 14 // yellow
 #define GPIO_MOSFET 15 // orange
 
-class sampling final {
-  static constexpr std::size_t size = 20;
+template<typename... X>
+class callback_manager;
+
+template<typename... Args>
+class callback_manager<void(Args...)> final {
 public:
-  void add(int64_t value) {
-    m_current = (m_current+1) % size;
-    m_values[m_current] = value;
+  void add(std::function<void(Args...)> &&callback) {
+    m_callbacks.emplace_back(std::move(callback));
   }
 
-  int64_t average() const {
-    int64_t sum = 0;
-    for (const auto &value : m_values) {
-      sum += value;
+  void call(Args... args) {
+    for (const auto &callback : m_callbacks) {
+      callback(args...);
     }
+  }
 
-    return sum / size;
+ private:
+  std::vector<std::function<void(Args...)>> m_callbacks;
+};
+
+class zero_crossing_detector final {
+public:
+  zero_crossing_detector(uint gpio) {
+    m_instance = this;
+
+    gpio_init(gpio);
+    gpio_set_dir(gpio, GPIO_IN);
+
+    // rising_per_sec rising per second
+    gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &s_irq_handler);
+  }
+
+  uint32_t period_us() const {
+    return m_period;
+  }
+
+  uint32_t zero_duration_us() const {
+    return m_zero_duration;
+  }
+
+  void register_callback(std::function<void()> &&callback) {
+    m_callback.add(std::move(callback));
   }
 
 private:
-  std::size_t m_current;
-  std::array<int64_t, size> m_values = {0};
+  void irq_handler(uint32_t events) {
+    auto now = get_absolute_time();
+    auto elapsed = absolute_time_diff_us(m_last_event, now);
+    m_last_event = now;
+
+    switch(events) {
+    case GPIO_IRQ_EDGE_RISE:
+      m_period = m_zero_duration + elapsed;
+      break;
+
+    case GPIO_IRQ_EDGE_FALL:
+      m_zero_duration = elapsed;
+      break;
+    }
+  }
+
+  static void s_irq_handler(uint gpio, uint32_t events) {
+    m_instance->irq_handler(events);
+  }
+
+  // this is ugly but we cannot pass args to irq handler
+  static zero_crossing_detector *m_instance;
+
+  absolute_time_t ABSOLUTE_TIME_INITIALIZED_VAR(m_last_event, 0);
+  uint32_t m_period = 0;
+  uint32_t m_zero_duration = 0;
+
+  callback_manager<void()> m_callback;
 };
 
-static volatile uint32_t rising_per_sec;
-static absolute_time_t last_ts;
-static sampling sampling_low;
-static sampling sampling_hi;
-
-static void gpio_cb(uint gpio, uint32_t events) {
-  auto now = get_absolute_time();
-  auto elapsed = absolute_time_diff_us(last_ts, now);
-  last_ts = now;
-
-  if (events == GPIO_IRQ_EDGE_RISE) {
-    ++rising_per_sec;
-
-    sampling_low.add(elapsed);
-  } else {
-    sampling_hi.add(elapsed);
-  }
-}
+zero_crossing_detector *zero_crossing_detector::m_instance = nullptr;
 
 int main() {
   bi_decl(bi_program_description("Zero crossing detector tests"));
@@ -60,21 +97,14 @@ int main() {
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-  gpio_init(GPIO_ZC);
-  gpio_set_dir(GPIO_ZC, GPIO_IN);
-
-  // rising_per_sec rising per second
-  gpio_set_irq_enabled_with_callback(GPIO_ZC, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_cb);
+  zero_crossing_detector zc(GPIO_ZC);
 
   bool led_state = false;
 
   while(true) {
     sleep_ms(1000);
 
-    auto value = rising_per_sec;
-    rising_per_sec = 0;
-
-    std::cout << "rising_per_sec: " << value << " Hz, low: " << sampling_low.average() << " us, hi: " << sampling_hi.average() << std::endl;
+    std::cout << "period: " << zc.period_us() << " us, zero duration: " << zc.zero_duration_us() << std::endl;
 
     gpio_put(PICO_DEFAULT_LED_PIN, led_state ? 1 : 0);
     led_state = !led_state;

fb43794bc3fda9d3653e48f4db9cf19ac67e14ad
vincent-tr <vincent.tr@gmail.com>
2022-04-03 16:57:56 +0200

    cleanup

--- button/zc.hh
+++ button/zc.hh
@@ -6,17 +6,16 @@
 
 class zero_crossing_detector final {
 public:
-  zero_crossing_detector(uint gpio)
-   : m_gpio(gpio) {
+  zero_crossing_detector(uint gpio) {
     m_instance = this;
 
     alarm_pool_init_default();
 
-    gpio_init(m_gpio);
-    gpio_set_dir(m_gpio, GPIO_IN);
+    gpio_init(gpio);
+    gpio_set_dir(gpio, GPIO_IN);
 
     // rising_per_sec rising per second
-    gpio_set_irq_enabled_with_callback(m_gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &s_irq_handler);
+    gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &s_irq_handler);
   }
 
   uint32_t period_us() const {
@@ -32,11 +31,7 @@ public:
   }
 
 private:
-  void irq_handler(uint gpio, uint32_t events) {
-    if (gpio != m_gpio) {
-      return;
-    }
-    
+  void irq_handler(uint32_t events) {
     auto now = get_absolute_time();
     auto elapsed = absolute_time_diff_us(m_last_event, now);
     m_last_event = now;
@@ -60,7 +55,7 @@ private:
   }
 
   static void s_irq_handler(uint gpio, uint32_t events) {
-    m_instance->irq_handler(gpio, events);
+    m_instance->irq_handler(events);
   }
 
   static int64_t s_alarm_trigger(alarm_id_t id, void *user_data) {
@@ -72,7 +67,6 @@ private:
   // this is ugly but we cannot pass args to irq handler
   static zero_crossing_detector *m_instance;
 
-  uint m_gpio;
   absolute_time_t ABSOLUTE_TIME_INITIALIZED_VAR(m_last_event, 0);
   uint32_t m_period = 0;
   uint32_t m_zero_duration = 0;

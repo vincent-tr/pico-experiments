2fd7851b41135a3f4eb385d491be9afa96c10d2e
vincent-tr <vincent.tr@gmail.com>
2022-08-30 16:46:35 +0200

    make example work

--- internal-temp/main.cc
+++ internal-temp/main.cc
@@ -12,15 +12,19 @@ int main() {
   stdio_init_all();
 
   adc_init();
+  adc_select_input(4);
+  adc_set_temp_sensor_enabled(true);
+
 
   while(true) {
     sleep_ms(1000);
 
-    adc_select_input(4);
-
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
     uint16_t result = adc_read();
-    printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
+    float adc_voltage = result * conversion_factor;
+    // T = 27 - (ADC_Voltage - 0.706)/0.001721
+    auto temp = 27 - (adc_voltage - 0.706) / 0.001721;
+    printf("Raw value: 0x%03x, voltage: %f V, temp: %f C\n", result, adc_voltage, temp);
   }
 }
